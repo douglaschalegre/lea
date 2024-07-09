d5c3e173cbf9ee9ae2e6499a67aa827fdad84eb1
douglaschalegre <douglas.chalegre@gmail.com>
2024-07-08 23:50:47 -0300

    refactor: Add form to add new student to the system

--- frontend/src/Students.tsx
+++ frontend/src/Students.tsx
@@ -1,37 +1,50 @@
 import { Button } from "./components/ui/button"
-import { Dialog, DialogTrigger, DialogContent, DialogTitle, DialogHeader, DialogDescription } from "./components/ui/dialog"
+import { Dialog, DialogTrigger, DialogContent, DialogTitle, DialogHeader, DialogDescription, DialogFooter, DialogClose } from "./components/ui/dialog"
 import { Table, TableCaption, TableHead, TableHeader, TableRow, TableBody, TableCell } from "./components/ui/table"
+import { Input } from "./components/ui/input"
 
-const mockData = [
+type Student = {
+    name: string
+    course: string
+    period: string
+    college: string
+    birthDate: number
+}
+
+const mockData: Student[] = [
   {
     name: "John Doe",
     course: "Computer Science",
     period: "Morning",
     college: "Harvard",
-    age: 21,
+    birthDate: 21,
   },
   {
     name: "Jane Doe",
     course: "Law",
     period: "Night",
     college: "Yale",
-    age: 23,
+    birthDate: 23,
   },
   {
     name: "Alice Doe",
     course: "Medicine",
     period: "Morning",
     college: "Stanford",
-    age: 25,
+    birthDate: 25,
   },
   {
     name: "Bob Doe",
     course: "Engineering",
     period: "Night",
     college: "MIT",
-    age: 22,
+    birthDate: 22,
   },
 ]
+
+function handleAdd({name, course, period, college, birthDate}: {name: string, course: string, period: string, college: string, birthDate: number}) {
+    mockData.push({ name , course, period, college, birthDate })
+}
 
 export function Students() {
   return (
@@ -50,9 +63,53 @@ export function Students() {
                         <DialogTitle>Adicionar aluno</DialogTitle>
                         <DialogDescription>Adicione um novo aluno ao sistema.</DialogDescription>
                     </DialogHeader>
-                    <div>
-                        
-                    </div>
+                    <form className="mt-2">
+                        <div className="grid grid-cols-6 gap-4 sm:grid-cols-4">
+                            <div className="col-span-4">
+                                <label htmlFor="name" className="block text-sm font-medium text-gray-700">Nome</label>
+                                <Input type="text" name="name" id="name" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+                            </div>
+                            <div className="col-span-2">
+                                <label htmlFor="bithdate" className="block text-sm font-medium text-gray-700">Data de nascimento</label>
+                                <Input type="text" placeholder="DD/MM/AAAA" onFocus={(e) => (e.target.type = "date")} onBlur={(e) => (e.target.type = "text")} name="bithdate" id="bithdate" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+                            </div>
+                            <div className="col-span-3">
+                                <label htmlFor="college" className="block text-sm font-medium text-gray-700">Faculdade</label>
+                                <Input type="text" name="college" id="college" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+                            </div>
+                            <div className="col-span-2">
+                                <label htmlFor="course" className="block text-sm font-medium text-gray-700">Curso</label>
+                                <Input type="text" name="course" id="course" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+                            </div>
+                            <div className="col-span-1">
+                                <label htmlFor="period" className="block text-sm font-medium text-gray-700">Período</label>
+                                <Input type="number" name="period" id="period" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+                            </div>
+                        </div>
+                        <DialogFooter className="">
+                            <div className="mt-8 flex gap-2 justify-end">
+                                <DialogClose asChild>
+                                    <Button variant="ghost" className="px-4 py-2 gap-2 sm:px-6">Cancelar</Button>
+                                </DialogClose>
+                                <Button
+                                  type="submit"
+                                  variant="default"
+                                  className="px-4 py-2 gap-2 sm:px-6"
+                                  onClick={e => {
+                                    handleAdd({
+                                      name: e.target.form.name.value,
+                                      course: e.target.form.course.value,
+                                      period: e.target.form.period.value,
+                                      college: e.target.form.college.value,
+                                      birthDate: e.target.form.bithdate.value
+                                    });
+                                  }}
+                                >
+                                  Adicionar
+                                </Button>
+                            </div>
+                        </DialogFooter>
+                    </form>
                 </DialogContent>
             </Dialog>
         </div>
@@ -75,7 +132,7 @@ export function Students() {
                 <TableCell>{data.course}</TableCell>
                 <TableCell>{data.period}</TableCell>
                 <TableCell>{data.college}</TableCell>
-                <TableCell>{data.age}</TableCell>
+                <TableCell>{data.birthDate}</TableCell>
             </TableRow>
         ))}
     </TableBody>
