7525ebf41c90585f1443ff0368cba96cc5c764e4
douglaschalegre <douglas.chalegre@gmail.com>
2024-08-29 17:51:34 -0300

    feat: add shadcnui

--- codex/components/AuthButton.tsx
+++ codex/components/AuthButton.tsx
@@ -1,6 +1,7 @@
 import { createClient } from "@/utils/supabase/server";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import { Button } from "./ui/button";
 
 export default async function AuthButton() {
   const supabase = createClient();
@@ -30,15 +31,17 @@ export default async function AuthButton() {
     <div className="flex gap-2">
       <Link
         href="/login"
-        className="h-8 flex items-center justify-center rounded-md no-underline text-sm font-medium px-4"
       >
-        Login
+        <Button variant='ghost'>
+          Entrar
+        </Button>
       </Link>
       <Link
         href="/signup"
-        className="h-8 flex items-center justify-center rounded-md no-underline bg-black text-white text-sm font-medium px-4"
       >
-        Sign up
+        <Button>
+          Registre-se
+        </Button>
       </Link>
     </div>
   );
