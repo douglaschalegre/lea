cc8edc50de60ec5c668c6c074f4e0b621c4cfd42
douglaschalegre <douglas.chalegre@gmail.com>
2024-08-31 20:46:11 -0300

    fix: main page text fix

--- codex/app/page.tsx
+++ codex/app/page.tsx
@@ -1,15 +1,16 @@
-import Link from "next/link";
-import Header from "@/components/Header";
-import { Button } from "@/components/ui/button";
+import Link from 'next/link';
+import Header from '@/components/Header';
+import { Button } from '@/components/ui/button';
 
 export default async function Index() {
-  
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
+    <div className="flex w-full flex-1 flex-col items-center gap-20">
+      <div className="flex max-w-4xl flex-1 flex-col gap-20 px-3">
         <Header />
-        <main className="flex-1 flex flex-col gap-6 px-4 items-center">
-          <h2 className="font-bold text-2xl mb-4">Inscreva-se agora e comece a ver vagas disponíveis!</h2>
+        <main className="mb-6 flex flex-1 flex-col items-center gap-6 px-4">
+          <h2 className="mb-4 text-center text-2xl font-bold">
+            Inscreva-se agora e comece a ver vagas disponíveis!
+          </h2>
           <Link href="/login">
             <Button>Registre-se</Button>
           </Link>
