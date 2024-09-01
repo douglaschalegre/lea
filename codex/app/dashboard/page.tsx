6e234cf12e439e8c629508702ff894b6c43ca1ee
douglaschalegre <douglas.chalegre@gmail.com>
2024-09-01 17:18:02 -0300

    refactor: dashboard update to remove header

--- codex/app/dashboard/page.tsx
+++ codex/app/dashboard/page.tsx
@@ -1,10 +1,13 @@
-import { createClient } from "@/utils/supabase/server";
-import Header from "@/components/Header";
-import { redirect } from "next/navigation";
-import CompleteRegistration from "@/components/CompleteRegistration";
-import { Message } from "@/components/forms/form-message";
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
+import CompleteRegistration from '@/components/CompleteRegistration';
+import { Message } from '@/components/forms/form-message';
 
-export default async function ProtectedPage({ searchParams }: { searchParams: Message }) {
+export default async function ProtectedPage({
+  searchParams,
+}: {
+  searchParams: Message;
+}) {
   const supabase = createClient();
 
   const {
@@ -12,19 +15,30 @@ export default async function ProtectedPage({ searchParams }: { searchParams: Me
   } = await supabase.auth.getUser();
 
   if (!user) {
-    return redirect("/login");
+    return redirect('/login');
   }
-  const {data: accountData, error} = await supabase.from('accounts').select('*').eq('auth_id', user.id).maybeSingle()
+  const { data: accountData, error } = await supabase
+    .from('accounts')
+    .select('*')
+    .eq('auth_id', user.id)
+    .maybeSingle();
   if (error) {
-    console.error("Error quering account data:" + error?.code + " " + error?.message);
+    console.error(
+      'Error quering account data:' + error?.code + ' ' + error?.message,
+    );
   }
-  if (accountData.filled_registration === false) return <CompleteRegistration searchParams={searchParams} accountId={accountData.id}/>
+  if (accountData.filled_registration === false)
+    return (
+      <CompleteRegistration
+        searchParams={searchParams}
+        accountId={accountData.id}
+      />
+    );
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
-        <Header />
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-2xl mb-4">Next steps</h2>
+    <div className="flex w-full flex-1 flex-col items-center gap-20">
+      <div className="flex max-w-4xl flex-1 flex-col gap-20 px-3">
+        <main className="flex flex-1 flex-col gap-6">
+          <h2 className="mb-4 text-2xl font-bold">Em desenvolvimento! 😅</h2>
         </main>
       </div>
     </div>
