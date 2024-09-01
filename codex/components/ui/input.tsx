fb5654ff8524f823b68bd6baac2e7f70ed583ff3
douglaschalegre <douglas.chalegre@gmail.com>
2024-09-01 16:45:57 -0300

    feat: add show password feature

--- codex/components/ui/input.tsx
+++ codex/components/ui/input.tsx
@@ -1,6 +1,7 @@
+'use client';
 import * as React from 'react';
-
 import { cn } from '@/utils/cn';
+import { EyeIcon, EyeOffIcon } from 'lucide-react';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface InputProps
@@ -8,6 +9,33 @@ export interface InputProps
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
+    const [showPassword, setShowPassword] = React.useState(false);
+    const togglePasswordVisibility = () => setShowPassword(!showPassword);
+    if (type == 'password') {
+      return (
+        <div className="relative">
+          <input
+            type={showPassword ? 'text' : 'password'}
+            className={cn(
+              'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+              className,
+            )}
+            ref={ref}
+            {...props}
+          />
+          <div className="absolute inset-y-0 right-0 flex cursor-pointer items-center pr-3 text-gray-400">
+            {showPassword ? (
+              <EyeOffIcon
+                className="h-4 w-4"
+                onClick={togglePasswordVisibility}
+              />
+            ) : (
+              <EyeIcon className="h-4 w-4" onClick={togglePasswordVisibility} />
+            )}
+          </div>
+        </div>
+      );
+    }
     return (
       <input
         type={type}
