daaa37792f65ee6fb8cb50ec2d776f558dc47d64
douglaschalegre <douglas.chalegre@gmail.com>
2024-09-01 16:59:04 -0300

    fix: remove wierd gap

--- codex/app/page.tsx
+++ codex/app/page.tsx
@@ -5,7 +5,7 @@ import { Button } from '@/components/ui/button';
 export default async function Index() {
   return (
     <div className="flex w-full flex-1 flex-col items-center gap-20">
-      <div className="flex max-w-4xl flex-1 flex-col gap-20 px-3">
+      <div className="flex max-w-4xl flex-1 flex-col px-3">
         <Header />
         <main className="mb-6 flex flex-1 flex-col items-center gap-6 px-4">
           <h2 className="mb-4 text-center text-2xl font-bold">
