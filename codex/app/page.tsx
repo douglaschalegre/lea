ab63a48ed6bbe8db6f426ffb3fdfc35ed942a808
douglaschalegre <douglas.chalegre@gmail.com>
2024-09-01 17:18:18 -0300

    feat: diferent actions to logged user

--- codex/app/page.tsx
+++ codex/app/page.tsx
@@ -1,19 +1,40 @@
 import Link from 'next/link';
+import { createClient } from '@/utils/supabase/server';
 import Header from '@/components/Header';
 import { Button } from '@/components/ui/button';
 
 export default async function Index() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   return (
     <div className="flex w-full flex-1 flex-col items-center gap-20">
       <div className="flex max-w-4xl flex-1 flex-col px-3">
         <Header />
         <main className="mb-6 flex flex-1 flex-col items-center gap-6 px-4">
-          <h2 className="mb-4 text-center text-2xl font-bold">
-            Inscreva-se agora e comece a ver vagas disponíveis!
-          </h2>
-          <Link href="/signup">
-            <Button>Registre-se</Button>
-          </Link>
+          {!user && (
+            <>
+              <h2 className="mb-4 text-center text-2xl font-bold">
+                Inscreva-se agora e comece a ver vagas disponíveis!
+              </h2>
+              <Link href="/signup">
+                <Button>Registre-se</Button>
+              </Link>
+            </>
+          )}
+          {user && (
+            <>
+              <h2 className="mb-4 text-center text-2xl font-bold">
+                Você já está logado, acesse a dashboard!
+              </h2>
+              <Link href="/dashboard">
+                <Button>Acessar</Button>
+              </Link>
+            </>
+          )}
         </main>
       </div>
     </div>
