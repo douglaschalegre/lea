d7a153ae57f81ac0eb28b18f5ed71ee943e4827e
douglaschalegre <douglas.chalegre@gmail.com>
2024-09-01 16:58:44 -0300

    feat: add text to companies

--- codex/components/Header.tsx
+++ codex/components/Header.tsx
@@ -5,7 +5,10 @@ import SuperEstagiosLogo from './SuperEstagiosLogo';
 export default function Header() {
   return (
     <div className="flex flex-col items-center gap-16">
-      <div className="flex items-center justify-center gap-8">
+      <h1 className="sr-only">
+        Plataforma Super Estágios, RHF Talentos e QCarreira, Maceió, AL.
+      </h1>
+      <div className="flex flex-col items-center justify-center">
         <a
           href="https://www.instagram.com/superestagios.al.maceio/"
           target="_blank"
@@ -13,7 +16,12 @@ export default function Header() {
         >
           <SuperEstagiosLogo />
         </a>
-        <span className="h-6 rotate-45 border-l" />
+        <p className="mx-auto max-w-xl text-center text-xl !leading-tight lg:text-4xl">
+          Especializada na administração de estágios, conectando estudantes a
+          empresas para oportunidades de aprendizado prático.
+        </p>
+      </div>
+      <div className="flex flex-col items-center justify-center">
         <a
           href="https://www.instagram.com/rhftalentos_maceio_jatiuca/"
           target="_blank"
@@ -21,48 +29,26 @@ export default function Header() {
         >
           <RhfLogo />
         </a>
-        <span className="h-6 rotate-45 border-l" />
+        <p className="mx-auto max-w-xl text-center text-xl !leading-tight lg:text-4xl">
+          Atua na seleção e recrutamento de profissionais, oferecendo soluções
+          de RH para empresas de diferentes portes e setores.
+        </p>
+      </div>
+      <div className="flex flex-col items-center justify-center">
         <a
           href="https://www.instagram.com/rhftalentos_maceio_jatiuca/"
           target="_blank"
           rel="noreferrer"
+          className="mb-3"
         >
           <QCarreiraLogo />
         </a>
+        <p className="mx-auto max-w-xl text-center text-xl !leading-tight lg:text-4xl">
+          Focada em desenvolvimento de carreira, oferece serviços de consultoria
+          e orientação para profissionais que buscam crescimento e transição na
+          carreira.
+        </p>
       </div>
-      <h1 className="sr-only">
-        Plataforma Super Estágios, RHF Talentos e QCarreira, Maceió, AL.
-      </h1>
-      <p className="mx-auto max-w-xl text-center text-3xl !leading-tight lg:text-4xl">
-        Recrutamento e seleção, treinamento, consultoria, RH e gestão é com{' '}
-        <a
-          href="https://www.instagram.com/superestagios.al.maceio/"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Super Estágios
-        </a>
-        ,{' '}
-        <a
-          href="https://www.instagram.com/rhftalentos_maceio_jatiuca/"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          RHF Talentos
-        </a>{' '}
-        e{' '}
-        <a
-          href="https://www.instagram.com/rhftalentos_maceio_jatiuca/"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          QCarreira
-        </a>
-        .
-      </p>
       <div className="my-8 w-full bg-gradient-to-r from-transparent via-foreground/10 to-transparent p-[1px]" />
     </div>
   );
